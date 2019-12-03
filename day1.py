0d8de3ee40a8ff3f045c76cbefa029ef1c2d3bb8
Jason Scheirer <jason.scheirer@gmail.com>
2019-12-03 09:47:54 -0800

    Two stars 1&2

--- day1.py
+++ day1.py
@@ -5,12 +5,21 @@ total = 0.0
 
 def fuel(mass):
     # Take its mass, divide by three, round down, and subtract 2.
-    return int(math.floor(float(mass) / 3.0) - 2.0)
+    return max([int(math.floor(float(mass) / 3.0) - 2.0), 0.0])
+
+
+def integrated_fuel(mass):
+    total_fuel = 0.0
+    more_fuel = fuel(mass)
+    while more_fuel > 0:
+        total_fuel += more_fuel
+        more_fuel = fuel(more_fuel)
+    return total_fuel
 
 
 with open("day1.input", "r") as in_file:
     for line in in_file:
         mass = float(line.strip())
-        total += fuel(mass)
+        total += integrated_fuel(mass)
 
 print(int(total))
--- day2.py
+++ day2.py
@@ -9,36 +9,38 @@ def chunks(line_array):
 
 
 def interpret_line(text_line):
-    line_array = [int(item) for item in text_line.strip().split(",")][:]
+    line_parsed = [int(item) for item in text_line.strip().split(",")]
     # Once you have a working computer, the first step is to restore the gravity
     # assist program (your puzzle input) to the "1202 program alarm" state it had
     # just before the last computer caught fire. To do this, before running the
     # program, replace position 1 with the value 12 and replace position 2 with the
     # value 2. What value is left at position 0 after the program halts?]
-    for x in range(0, 99):
-        for y in range(0, 99):
-            line_array[1] = x
-            line_array[2] = y
-            for chunk in chunks(line_array):
-                opcode, *others = chunk
-                print(opcode, others)
-                if opcode == 99:
-                    break
-                elif opcode in (1, 2):
-                    addr1, addr2, dest = others
-                    operand = {1: lambda x, y: x + y,
-                               2: lambda x, y: x*y}[opcode]
-                    print(line_array[dest])
-                    line_array[dest] = operand(
-                        line_array[addr1], line_array[addr2])
-                else:
-                    pass
-                    # raise ValueError(opcode)
-            if line_array[0] == 19690720:
-                yield x, y, line_array[0]
+    for x in range(100):
+        for y in range(100):
+            line_array = line_parsed[:]
+            try:
+                line_array[1] = x
+                line_array[2] = y
+                for chunk in chunks(line_array):
+                    opcode, *others = chunk
+                    if opcode == 99:
+                        break
+                    elif opcode in (1, 2):
+                        addr1, addr2, dest = others
+                        operand = {1: lambda x, y: x + y,
+                                   2: lambda x, y: x*y}[opcode]
+                        line_array[dest] = operand(
+                            line_array[addr1], line_array[addr2])
+                    else:
+                        pass
+                        # raise ValueError(opcode)
+                if line_array[0] == 19690720:
+                    yield x, y, line_array[0]
+            except Exception:
+                pass
 
 
 with open("day2.input", "r") as in_lines:
     for line in in_lines:
         for item in interpret_line(line):
-            print(item)
+            print(100 * item[0] + item[1])
